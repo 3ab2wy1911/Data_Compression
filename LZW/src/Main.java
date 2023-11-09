dd6dd0b930120498d5322317552012de0aad755b
3ab2wy1911 <3ab2wy1911@gmail.com>
2023-11-09 02:06:32 +0200

    Update LZW

--- LZW/src/Main.java
+++ LZW/src/Main.java
@@ -1,24 +1,38 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // Two files one for compression and other for decompression.
-        String pathCompress = "E:\\PROJECTS\\Data Compression\\LZW\\Compression.txt";
-        String pathDecompress = "E:\\PROJECTS\\Data Compression\\LZW\\Decompression.txt";
+        String pathCompress = "E:\\PROJECTS\\Data Compression\\Data_Compression\\LZW\\Compression.txt";
+        String pathDecompress = "E:\\PROJECTS\\Data Compression\\Data_Compression\\LZW\\Decompression.txt";
 
         // Making two instances of manager one for compression and other for decompression.
         Manager managerCompress = new Manager (pathCompress);
         Manager managerDecompress = new Manager (pathDecompress);
 
-        // Compression Process.
-        managerCompress.readFile();
-        Compression compression = new Compression (managerCompress.getText());
-        managerDecompress.setText(compression.compress().toString());
-        managerDecompress.writeFile();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                Please enter your choice:
+                1. Compression
+                2. Decompression""");
+        int input = scanner.nextInt();
+        if (input == 1){
+            // Compression Process.
+            managerCompress.readFile();
+            Compression compression = new Compression (managerCompress.getText());
+            managerDecompress.setText(compression.compress().toString());
+            managerDecompress.writeFile();
+        }
 
-        // Decompression Process.
-        managerDecompress.readFile();
-        Decompression decompression = new Decompression (managerDecompress.getText());
-        managerCompress.setText(decompression.decompress());
-        managerCompress.writeFile();
+        else if (input == 2){
+            // Decompression Process.
+            managerDecompress.readFile();
+            Decompression decompression = new Decompression (managerDecompress.getText());
+            managerCompress.setText(decompression.decompress());
+            managerCompress.writeFile();
+        }
+        else {
+            System.out.println("Wrong Input !!!");
+        }
     }
 }
